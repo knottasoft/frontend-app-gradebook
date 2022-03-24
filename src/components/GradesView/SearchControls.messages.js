569c0ffa9167310403336f2af3e718fdb82a7d87
Artem <artem.marshalkin@icloud.com>
2022-03-24 19:46:58 +0300

    translate

--- src/components/GradesView/SearchControls.messages.js
+++ src/components/GradesView/SearchControls.messages.js
@@ -3,12 +3,12 @@ import { defineMessages } from '@edx/frontend-platform/i18n';
 const messages = defineMessages({
   label: {
     id: 'gradebook.GradesView.search.label',
-    defaultMessage: 'Search for a learner',
+    defaultMessage: 'Поиск обучающегося',
     description: 'Text prompting a user to use this functionality to search for a learner',
   },
   hint: {
     id: 'gradebook.GradesView.search.hint',
-    defaultMessage: 'Search by username, email, or student key',
+    defaultMessage: 'Поиск по имени пользователя, электронной почте или студенческому ключу',
     description: 'A hint explaining the ways a user can search',
   },
 });
