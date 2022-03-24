2dcc68b8953286975004262abfc69334750308b1
Artem <artem.marshalkin@icloud.com>
2022-03-24 19:46:19 +0300

    translate

--- src/components/GradesView/ScoreViewInput.messages.js
+++ src/components/GradesView/ScoreViewInput.messages.js
@@ -3,17 +3,17 @@ import { defineMessages } from '@edx/frontend-platform/i18n';
 const messages = defineMessages({
   scoreView: {
     id: 'gradebook.GradesView.scoreViewLabel',
-    defaultMessage: 'Score View',
+    defaultMessage: 'Просмотр оценок',
     description: 'The label for the dropdown list that allows a user to select the Score format',
   },
   absolute: {
     id: 'gradebook.GradesView.absoluteOption',
-    defaultMessage: 'Absolute',
+    defaultMessage: 'Абсолютное значение',
     description: 'A label within the Score Format dropdown list for the Absolute Grade Score option',
   },
   percent: {
     id: 'gradebook.GradesView.percentOption',
-    defaultMessage: 'Percent',
+    defaultMessage: 'Процент',
     description: 'A label within the Score Format dropdown list for the Percent Grade Score option',
   },
 });
