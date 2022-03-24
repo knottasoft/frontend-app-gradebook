904ed22746e5c48a273bb7ed27315aee6d9d4aa8
Artem <artem.marshalkin@icloud.com>
2022-03-24 19:45:17 +0300

    Update InterventionsReport.messages.js

--- src/components/GradesView/InterventionsReport.messages.js
+++ src/components/GradesView/InterventionsReport.messages.js
@@ -3,17 +3,17 @@ import { defineMessages } from '@edx/frontend-platform/i18n';
 const messages = defineMessages({
   title: {
     id: 'gradebook.GradesView.InterventionsReport.title',
-    defaultMessage: 'Interventions Report',
+    defaultMessage: 'Отчет о вмешательствах',
     description: 'The title for the Intervention report subsection',
   },
   description: {
     id: 'gradebook.GradesView.InterventionsReport.description',
-    defaultMessage: 'Need to find students who may be falling behind?  Download the interventions report to obtain engagement metrics such as section attempts and visits.',
+    defaultMessage: 'Нужно найти студентов, которые, возможно, отстают?  Загрузите отчет о вмешательствах, чтобы получить показатели вовлеченности, такие как попытки и посещения разделов.',
     description: 'The description for the Intervention report subsection',
   },
   downloadBtn: {
     id: 'gradebook.GradesView.InterventionsReport.downloadBtn',
-    defaultMessage: 'Download Interventions',
+    defaultMessage: 'Скачать вмешательства',
     description: 'The labeled button to download the Intervention report from the Grades View',
   },
 });
