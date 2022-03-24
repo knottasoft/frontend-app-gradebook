e44d06680eb3296c9585a4cb2ce29b04ed25f8ea
Artem <artem.marshalkin@icloud.com>
2022-03-24 19:42:16 +0300

    Update FilterMenuToggle.messages.js

--- src/components/GradesView/FilterMenuToggle.messages.js
+++ src/components/GradesView/FilterMenuToggle.messages.js
@@ -3,7 +3,7 @@ import { defineMessages } from '@edx/frontend-platform/i18n';
 const messages = defineMessages({
   editFilters: {
     id: 'gradebook.GradesView.editFilterLabel',
-    defaultMessage: 'Edit Filters',
+    defaultMessage: 'Редактировать фильтры',
     description: 'A labeled button in the Grades tab that opens/closes the Filters tab, allowing the grades to be filtered',
   },
 });
