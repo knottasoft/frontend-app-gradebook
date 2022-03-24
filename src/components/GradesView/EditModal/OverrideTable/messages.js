8c420e0ec7a6590d290e9776f39d500b2c07da08
Artem <artem.marshalkin@icloud.com>
2022-03-24 19:36:36 +0300

    ttanslate

--- src/components/GradesView/EditModal/OverrideTable/messages.js
+++ src/components/GradesView/EditModal/OverrideTable/messages.js
@@ -3,22 +3,22 @@ import { defineMessages } from '@edx/frontend-platform/i18n';
 const messages = defineMessages({
   adjustedGradeHeader: {
     id: 'gradebook.GradesView.EditModal.Overrides.adjustedGradeHeader',
-    defaultMessage: 'Adjusted grade',
+    defaultMessage: 'Скорректированная оценка',
     description: 'Edit Modal Override Table Adjusted grade column header',
   },
   dateHeader: {
     id: 'gradebook.GradesView.EditModal.Overrides.dateHeader',
-    defaultMessage: 'Date',
+    defaultMessage: 'Дата',
     description: 'Edit Modal Override Table Date column header',
   },
   graderHeader: {
     id: 'gradebook.GradesView.EditModal.Overrides.graderHeader',
-    defaultMessage: 'Grader',
+    defaultMessage: 'Градация',
     description: 'Edit Modal Override Table Grader column header',
   },
   reasonHeader: {
     id: 'gradebook.GradesView.EditModal.Overrides.reasonHeader',
-    defaultMessage: 'Reason',
+    defaultMessage: 'Причина',
     description: 'Edit Modal Override Table Reason column header',
   },
 });
