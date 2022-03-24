e07e2a3b27b35c4d13963cd2b64f1c463c839762
Artem <artem.marshalkin@icloud.com>
2022-03-24 19:43:04 +0300

    Update ImportGradesButton.messages.js

--- src/components/GradesView/ImportGradesButton.messages.js
+++ src/components/GradesView/ImportGradesButton.messages.js
@@ -3,12 +3,12 @@ import { defineMessages } from '@edx/frontend-platform/i18n';
 const messages = defineMessages({
   csvUploadLabel: {
     id: 'gradebook.BulkManagementHistoryView.csvUploadLabel',
-    defaultMessage: 'Upload Grade CSV',
+    defaultMessage: 'Загрузите CSV с оценками',
     description: 'A labeled button to upload a CSV containing course grades.',
   },
   importGradesBtnText: {
     id: 'gradebook.GradesView.importGradesBtnText',
-    defaultMessage: 'Import Grades',
+    defaultMessage: 'Импортировать оценки',
     description: 'A labeled button to import grades in the BulkManagement Tab File Upload Form',
   },
 });
