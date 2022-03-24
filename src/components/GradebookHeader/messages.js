96397ef239061a83ea8d9f166e07fb61eb83638e
Artem <artem.marshalkin@icloud.com>
2022-03-24 19:32:17 +0300

    translate

--- src/components/GradebookHeader/messages.js
+++ src/components/GradebookHeader/messages.js
@@ -3,32 +3,32 @@ import { defineMessages } from '@edx/frontend-platform/i18n';
 const messages = defineMessages({
   backToDashboard: {
     id: 'gradebook.GradebookHeader.backButton',
-    defaultMessage: 'Back to Dashboard',
+    defaultMessage: 'Назад к панели инструментов',
     description: 'Button text to take user back to LMS dashboard in Gradebook Header',
   },
   gradebook: {
     id: 'gradebook.GradebookHeader.appLabel',
-    defaultMessage: 'Gradebook',
+    defaultMessage: 'Журнал успеваемости',
     description: 'Top-level app title in Gradebook Header component',
   },
   frozenWarning: {
     id: 'gradebook.GradebookHeader.frozenWarning',
-    defaultMessage: 'The grades for this course are now frozen. Editing of grades is no longer allowed.',
+    defaultMessage: 'Оценки по этому предмету теперь заблокированы. Редактирование оценок больше не допускается.',
     description: 'Warning message in Gradebook Header for frozen messages',
   },
   unauthorizedWarning: {
     id: 'gradebook.GradebookHeader.unauthorizedWarning',
-    defaultMessage: 'You are not authorized to view the gradebook for this course.',
+    defaultMessage: 'Вы не имеете права просматривать журнал оценок по этому курсу.',
     description: 'Warning message in Gradebook Header when user is not allowed to view the app',
   },
   toActivityLog: {
     id: 'gradebook.GradebookHeader.toActivityLogButton',
-    defaultMessage: 'View Bulk Management History',
+    defaultMessage: 'Просмотр истории массового управления',
     description: 'Button text for button navigating to Bulk Managment Activity Log',
   },
   toGradesView: {
     id: 'gradebook.GradebookHeader.toGradesView',
-    defaultMessage: 'Return to Gradebook',
+    defaultMessage: 'Вернуться в журнал успеваемости',
     description: 'Button text for button navigating to Grades view.',
   },
 });
