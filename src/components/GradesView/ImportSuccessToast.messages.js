5ca7b36ee3fbcdd6669c3572a619158a916fcb91
Artem <artem.marshalkin@icloud.com>
2022-03-24 19:43:38 +0300

    Update ImportSuccessToast.messages.js

--- src/components/GradesView/ImportSuccessToast.messages.js
+++ src/components/GradesView/ImportSuccessToast.messages.js
@@ -3,12 +3,12 @@ import { defineMessages } from '@edx/frontend-platform/i18n';
 const messages = defineMessages({
   description: {
     id: 'gradebook.GradesView.ImportSuccessToast.description',
-    defaultMessage: 'Import Successful! Grades will be updated momentarily.',
+    defaultMessage: 'Импорт прошел успешно! Оценки будут обновлены в ближайшее время.',
     description: 'A message congratulating a successful Import of grades',
   },
   showHistoryViewBtn: {
     id: 'gradebook.GradesView.ImportSuccessToast.showHistoryViewBtn',
-    defaultMessage: 'View Activity Log',
+    defaultMessage: 'Просмотр журнала активности',
     description: 'The text on a button that loads a view of the Bulk Management Activity Log',
   },
 });
