1305fce41eac878183cfaa19ed009a48033fee83
Artem <artem.marshalkin@icloud.com>
2022-03-24 19:38:54 +0300

    translate

--- src/components/GradesView/GradebookTable/messages.js
+++ src/components/GradesView/GradebookTable/messages.js
@@ -3,32 +3,32 @@ import { defineMessages } from '@edx/frontend-platform/i18n';
 const messages = defineMessages({
   emailHeading: {
     id: 'gradebook.GradesView.table.headings.email',
-    defaultMessage: 'Email',
+    defaultMessage: 'Электронная почта',
     description: 'Gradebook table email column header',
   },
   totalGradeHeading: {
     id: 'gradebook.GradesView.table.headings.totalGrade',
-    defaultMessage: 'Total Grade (%)',
+    defaultMessage: 'Итоговая оценка (%)',
     description: 'Gradebook table total grade column header',
   },
   usernameHeading: {
     id: 'gradebook.GradesView.table.headings.username',
-    defaultMessage: 'Username',
+    defaultMessage: 'Имя пользователя',
     description: 'Gradebook table username column header',
   },
   studentKeyLabel: {
     id: 'gradebook.GradesView.table.labels.studentKey',
-    defaultMessage: 'Student Key*',
+    defaultMessage: 'Ключ студента*',
     description: 'Gradebook table Student Key label',
   },
   usernameLabel: {
     id: 'gradebook.GradesView.table.labels.username',
-    defaultMessage: 'Username',
+    defaultMessage: 'Имя пользователя',
     description: 'Gradebook table username label',
   },
   totalGradePercentage: {
     id: 'gradebook.GradesView.table.totalGradePercentage',
-    defaultMessage: 'Total Grade values are always displayed as a percentage',
+    defaultMessage: 'Итоговые значения оценок всегда отображаются в процентах',
     description: 'Gradebook table message that total grades are displayed in percent format',
   },
 });
