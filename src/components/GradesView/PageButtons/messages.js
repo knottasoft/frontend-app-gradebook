9d3ebe69680f9c157485efb6d9881f40f0fe6533
Artem <artem.marshalkin@icloud.com>
2022-03-24 19:40:01 +0300

    translate

--- src/components/GradesView/PageButtons/messages.js
+++ src/components/GradesView/PageButtons/messages.js
@@ -3,12 +3,12 @@ import { defineMessages } from '@edx/frontend-platform/i18n';
 const messages = defineMessages({
   prevPage: {
     id: 'gradebook.GradesView.PageButtons.prevPage',
-    defaultMessage: 'Previous Page',
+    defaultMessage: 'Предыдущая страница',
     description: 'Grades tab Previous Page button text',
   },
   nextPage: {
     id: 'gradebook.GradesView.PageButtons.nextPage',
-    defaultMessage: 'Next Page',
+    defaultMessage: 'Следующая страница',
     description: 'Grades tab Next Page button text',
   },
 });
