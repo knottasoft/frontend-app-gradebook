40d7f4e6ecd2b15307dba7cf96a9327c5dd86914
Artem <artem.marshalkin@icloud.com>
2022-03-25 00:36:16 +0300

    Update grades.js

--- src/data/constants/grades.js
+++ src/data/constants/grades.js
@@ -5,8 +5,8 @@ const TOTAL_COURSE_GRADE_HEADING = 'Итоговая оценка (%)';
 const USERNAME_HEADING = 'Имя пользователя';
 
 const GradeFormats = StrictDict({
-  absolute: 'абсолютное значение',
-  percent: 'процент',
+  absolute: 'absolute',
+  percent: 'percent',
 });
 
 const Headings = StrictDict({
