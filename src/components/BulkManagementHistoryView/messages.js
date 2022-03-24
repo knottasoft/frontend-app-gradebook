346b7c067f8c700bf5cb7b5bb4b2670c74c995a7
Artem <artem.marshalkin@icloud.com>
2022-03-24 19:24:31 +0300

    translate

--- src/components/BulkManagementHistoryView/messages.js
+++ src/components/BulkManagementHistoryView/messages.js
@@ -3,17 +3,17 @@ import { defineMessages } from '@edx/frontend-platform/i18n';
 const messages = defineMessages({
   heading: {
     id: 'gradebook.BulkManagementHistoryView.heading',
-    defaultMessage: 'Bulk Management History',
+    defaultMessage: 'История массового управления',
     description: 'Heading text for BulkManagement History Tab',
   },
   helpText: {
     id: 'gradebook.BulkManagementHistoryView',
-    defaultMessage: 'Below is a log of previous grade imports.  To download a CSV of your gradebook and import grades for override, return to the Gradebook.  Please note, after importing grades, it may take a few seconds to process the override.',
+    defaultMessage: 'Ниже приведен журнал предыдущих импортов оценок.  Чтобы загрузить CSV-файл вашего журнала успеваемости и импортировать оценки для переопределения, вернитесь в журнал успеваемости.  Обратите внимание, что после импорта оценок может потребоваться несколько секунд для обработки переопределения.',
     description: 'Bulk Management History View help text',
   },
   successDialog: {
     id: 'gradebook.BulkManagementHistoryView.successDialog',
-    defaultMessage: 'CSV processing. File uploads may take several minutes to complete.',
+    defaultMessage: 'Обработка CSV. Загрузка файлов может занять несколько минут.',
     description: 'Success Dialog message in BulkManagement Tab File Upload Form',
   },
 });
