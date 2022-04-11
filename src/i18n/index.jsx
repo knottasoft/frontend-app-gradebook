80a280ec6c8eda5652c3af113cb7383b23041d74
Artem <artem.marshalkin@gmail.com>
2022-04-11 17:20:51 +0300

    Ticket PLPE-14

--- src/i18n/index.jsx
+++ src/i18n/index.jsx
@@ -3,12 +3,14 @@ import arMessages from './messages/ar.json';
 import es419Messages from './messages/es_419.json';
 import frMessages from './messages/fr.json';
 import zhcnMessages from './messages/zh_CN.json';
+import ruMessages from './messages/ru.json'
 
 const messages = {
   ar: arMessages,
   'es-419': es419Messages,
   fr: frMessages,
   'zh-cn': zhcnMessages,
+  'ru': ruMessages
 };
 
 export default messages;
